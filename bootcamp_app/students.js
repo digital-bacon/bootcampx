093e7d0302ae65aa4bfdaaa031f889498d9d527d
Mark Bieganek <mark.bieganek@telus.com>
2022-12-14 15:03:25 -0700

    Modify students query to accept command line arguments

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,15 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const [ execPath, filePath, cohortName, limitResults ] = process.argv;
+
+const sqlText = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-LIMIT 5;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name = $1
+LIMIT $2;
+`;
+const sqlValues = [cohortName, (limitResults ? limitResults : 5)];
+
+pool
+  .query(sqlText, sqlValues)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
   })
-})
-.catch(err => console.error('query error', err.stack));
+  .catch(err => console.error('query error', err.stack))
+  .finally(pool.end())
+  .then(() => console.log('>> pool has ended'));
