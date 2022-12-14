3b966748840a45135a4589c425c1c890eb7fd505
Mark Bieganek <mark.bieganek@telus.com>
2022-12-14 15:06:33 -0700

    Change query parameters to an object

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,17 +9,19 @@ const pool = new Pool({
 
 const [ execPath, filePath, cohortName, limitResults ] = process.argv;
 
-const sqlText = `
+const query = {
+  text: `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = students.cohort_id
 WHERE cohorts.name = $1
 LIMIT $2;
-`;
-const sqlValues = [cohortName, (limitResults ? limitResults : 5)];
+`,
+  values: [cohortName, (limitResults ? limitResults : 5)],
+}
 
 pool
-  .query(sqlText, sqlValues)
+  .query(query)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
